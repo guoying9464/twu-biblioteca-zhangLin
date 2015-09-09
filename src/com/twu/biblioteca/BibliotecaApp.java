81876824262db0dbb32a413b9a0be09d1bdac79a
Zhang Lin <lin.zhang@rea-group.com>
2015-09-10 00:32:59 +0800

    [Story-3] show book details

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,32 +1,53 @@
 package com.twu.biblioteca;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BibliotecaApp {
 
-    public static final String WELCOME_MESSAGE = "Welcome to Bibliteca!";
+    public static final String WELCOME_MESSAGE = "Welcome to Bibliteca!\n";
 
     public static void main(String[] args) {
         List<String> bookList = new ArrayList<>();
-        bookList.add("BookList:");
-        bookList.add("The Adventures of Sherlock Holmes");
-        bookList.add("Grimm's Fairy Tales");
-        bookList.add("Wuthering Heights");
-        bookList.add("Moon and Sixpence");
+        bookList.add("BookList:\n");
+        bookList.add("Grimm's Fairy Tales\n");
+        bookList.add("Wuthering Heights\n");
+        bookList.add("Moon and Sixpence\n");
+
+        Map<String, List<String>> map = new HashMap<>();
+        map.put("Grimm's Fairy Tales\t", addBookInfo("Jacob Grimm\t", "2011\t\n"));
+        map.put("Wuthering Heights\t", addBookInfo("Emily Bronte\t", "2011\t\n"));
+        map.put("Moon and Sixpence\t", addBookInfo("W. Somerset\t", "2012\t\n"));
 
         Printer printer = new Printer();
         printer.print(WELCOME_MESSAGE);
         printer.print(bookList);
+        printer.print(map);
+    }
+
+    private static List addBookInfo(String author, String year){
+        List list =  new ArrayList<String>();
+        list.add(author);
+        list.add(year);
+        return list;
     }
 
     public static class Printer{
         public void print(String message){
-            System.out.println(message);
+            System.out.print(message);
         }
 
         public void print(List<String> strings){
-            strings.stream().forEach(book -> System.out.println(book));
+            strings.stream().forEach(book -> print(book));
+        }
+
+        public void print(Map<String, List<String>> map){
+            map.entrySet().stream().forEach(entry -> {
+                print(entry.getKey());
+                print(entry.getValue());
+            });
         }
     }
 
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -7,13 +7,15 @@ import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class ExampleTest {
-    private static final String WELCOME_MESSAGE = "Welcome to Bibliteca!";
+    private static final String WELCOME_MESSAGE = "Welcome to Bibliteca!\n";
     private ByteArrayOutputStream output;
     private BibliotecaApp.Printer printer;
     List<String> bookList = new ArrayList<>();
@@ -24,11 +26,6 @@ public class ExampleTest {
         System.setOut(new PrintStream(output));
         printer = new BibliotecaApp.Printer();
 
-        bookList.add("BookList:");
-        bookList.add("The Adventures of Sherlock Holmes");
-        bookList.add("Grimm's Fairy Tales");
-        bookList.add("Wuthering Heights");
-        bookList.add("Moon and Sixpence");
     }
 
     @Test
@@ -39,12 +36,38 @@ public class ExampleTest {
 
     @Test
     public void testStringsPrinter() throws Exception {
+        bookList.add("BookList:\n");
+        bookList.add("Grimm's Fairy Tales\n");
+        bookList.add("Wuthering Heights\n");
+        bookList.add("Moon and Sixpence\n");
+
         String expect = "BookList:\n" +
-                "The Adventures of Sherlock Holmes\n" +
                 "Grimm's Fairy Tales\n" +
                 "Wuthering Heights\n" +
                 "Moon and Sixpence\n";
         printer.print(bookList);
         assertThat(output.toString(), is(expect));
+    }
+
+    @Test
+    public void testMapPrinter() throws Exception {
+        Map<String, List<String>> map = new HashMap<>();
+        map.put("Grimm's Fairy Tales\t",addBookInfo("Jacob Grimm\t", "2011\t\n"));
+        map.put("Wuthering Heights\t",addBookInfo("Emily Bronte\t", "2011\t\n"));
+        map.put("Moon and Sixpence\t", addBookInfo("W. Somerset\t", "2012\t\n"));
+
+        String expect = "Wuthering Heights\tEmily Bronte\t2011\t\n" +
+                "Moon and Sixpence\tW. Somerset\t2012\t\n" +
+                "Grimm's Fairy Tales\tJacob Grimm\t2011\t\n";
+
+        printer.print(map);
+        assertThat(output.toString(), is(expect));
+    }
+
+    private List addBookInfo(String author, String year){
+        List list =  new ArrayList<String>();
+        list.add(author);
+        list.add(year);
+        return list;
     }
 }
