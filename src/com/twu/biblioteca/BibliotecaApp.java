d62ad417b73f36592816ed397f56b36b63f41c77
Zhang Lin <lin.zhang@rea-group.com>
2015-09-10 00:00:20 +0800

    Use Printer to print different info

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -5,22 +5,29 @@ import java.util.List;
 
 public class BibliotecaApp {
 
+    public static final String WELCOME_MESSAGE = "Welcome to Bibliteca!";
+
     public static void main(String[] args) {
-        WelcomeMessagePrinter();
-        BookList();
-    }
-
-    public static void WelcomeMessagePrinter() {
-        System.out.println("Welcome to Bibliteca!");
-    }
-
-    public static void BookList(){
         List<String> bookList = new ArrayList<>();
         bookList.add("BookList:");
         bookList.add("The Adventures of Sherlock Holmes");
         bookList.add("Grimm's Fairy Tales");
         bookList.add("Wuthering Heights");
         bookList.add("Moon and Sixpence");
-        bookList.stream().forEach(book -> System.out.println(book));
+
+        Printer printer = new Printer();
+        printer.print(WELCOME_MESSAGE);
+        printer.print(bookList);
     }
+
+    public static class Printer{
+        public void print(String message){
+            System.out.println(message);
+        }
+
+        public void print(List<String> strings){
+            strings.stream().forEach(book -> System.out.println(book));
+        }
+    }
+
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -6,33 +6,45 @@ import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class ExampleTest {
+    private static final String WELCOME_MESSAGE = "Welcome to Bibliteca!";
     private ByteArrayOutputStream output;
+    private BibliotecaApp.Printer printer;
+    List<String> bookList = new ArrayList<>();
 
     @Before
     public void setUp() throws Exception {
         output = new ByteArrayOutputStream();
         System.setOut(new PrintStream(output));
+        printer = new BibliotecaApp.Printer();
+
+        bookList.add("BookList:");
+        bookList.add("The Adventures of Sherlock Holmes");
+        bookList.add("Grimm's Fairy Tales");
+        bookList.add("Wuthering Heights");
+        bookList.add("Moon and Sixpence");
     }
 
     @Test
-    public void testWelcomeMessage() {
-        BibliotecaApp.WelcomeMessagePrinter();
+    public void testMessagePrinter() {
+        printer.print(WELCOME_MESSAGE);
         assertThat(output.toString(), is("Welcome to Bibliteca!\n"));
     }
 
     @Test
-    public void testBookList() throws Exception {
+    public void testStringsPrinter() throws Exception {
         String expect = "BookList:\n" +
                 "The Adventures of Sherlock Holmes\n" +
                 "Grimm's Fairy Tales\n" +
                 "Wuthering Heights\n" +
                 "Moon and Sixpence\n";
-        BibliotecaApp.BookList();
+        printer.print(bookList);
         assertThat(output.toString(), is(expect));
     }
 }
