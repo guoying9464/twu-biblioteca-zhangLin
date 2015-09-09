471f0d0bc3a5708f2e2bb0df803018f3a4583349
Zhang Lin <lin.zhang@rea-group.com>
2015-09-09 23:10:21 +0800

    [Story-1] show welcome message

--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,29 @@
 package com.twu.biblioteca;
 
 
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class ExampleTest {
+    private ByteArrayOutputStream output;
+
+    @Before
+    public void setUp() throws Exception {
+        output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+    }
 
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void testWelcomeMessage() {
+        String[] args = {};
+        BibliotecaApp.main(args);
+
+        assertThat(output.toString(), is("Welcome to Bibliteca!"));
     }
 }
