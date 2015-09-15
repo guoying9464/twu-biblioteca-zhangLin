5e55b3fd24b43fa69d42fcf988216a5ac4687476
Zhang Lin <lin.zhang@rea-group.com>
2015-09-15 08:45:01 +0800

    [story-4] add Menu

---
+++ src/com/twu/biblioteca/Menu.java
@@ -0,0 +1,22 @@
+package com.twu.biblioteca;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Menu {
+    private Map<Integer, String> menu;
+
+    public Menu(Map<Integer, String> menu) {
+        this.menu = menu;
+    }
+
+    @Override
+    public String toString() {
+        List<String> result = new ArrayList();
+        menu.entrySet().stream().forEach(entry ->
+                result.add(String.format("%d\t%s",
+                        entry.getKey(), entry.getValue())));
+        return result.toString();
+    }
+}
